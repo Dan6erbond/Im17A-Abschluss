48076e036e3a36d266b9014502aabb09955c8dd3
Dan6erbond <moravrav@gmail.com>
2020-05-15 14:30:26 +0200

    Remove template project files
     - Remove template project files
     - Add hashRouter and switch for teachers

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Switch} from "react-router";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <Route path="/bLUb5kH">
+                <div>Matthias Graf</div>
+            </Route>
+            <Route path="/N6bmxqI">
+                <div>Marielle Guignard</div>
+            </Route>
+            <Route path="/SWH6Yjq">
+                <div>Lil Klink</div>
+            </Route>
+            <Route path="/EkmjaQB">
+                <div>Loredana Arleo</div>
+            </Route>
+            <Route path="/dshb4LF">
+                <div>Daniel Schneeberger</div>
+            </Route>
+            <Route path="/hJAQqe4">
+                <div>Genevieve Gross</div>
+            </Route>
+            <Route path="/eccFgkV">
+                <div>Andreas Neeser</div>
+            </Route>
+            <Route path="/DSraT8n">
+                <div>Sascha Fiechter</div>
+            </Route>
+            <Route path="/arJqAVL">
+                <div>Fabian Jerg</div>
+            </Route>
+            <Route path="/cii6Kwf">
+                <div>Alexander Flick</div>
+            </Route>
+            <Route path="/iy4e0Rr">
+                <div>Lars Meyer</div>
+            </Route>
+            <Route path="/5rM6ams">
+                <div>Réne Weidmann</div>
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
