602cbbce9eea8101b94d0816ba23ef64e9689c16
Dan6erbond <moravrav@gmail.com>
2020-05-15 22:09:27 +0200

    Update styling of home component for temporary usability

--- src/src/pages/Home/Home.tsx
+++ src/src/pages/Home/Home.tsx
@@ -6,10 +6,12 @@ import {Link} from "react-router-dom";
 export default function Home() {
     return (
         <React.Fragment>
-            <Container fluid="md">
+            <Container fluid="md" style={{padding: '15px'}}>
+                <h2 style={{textAlign: 'center'}}>Die Im17A ist fertig mit der Schule!</h2>
+                <br/>
                 <Row>
-                    {teachers.map((t, i) => <Col key={i} md={3} sm={6}>
-                        <Link to={t.path}>{t.name}</Link>
+                    {teachers.map((t, i) => <Col style={{textAlign: 'center', padding: '15px'}} key={i} md={3} sm={6}>
+                        <Link className="btn btn-outline-dark" to={t.path}>{t.name}</Link>
                     </Col>)}
                 </Row>
             </Container>
