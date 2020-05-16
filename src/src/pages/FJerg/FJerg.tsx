a137e964b1065ab9ec47be91ca7fa9930d54a3e6
Dan6erbond <moravrav@gmail.com>
2020-05-16 08:51:13 +0200

    Closes #38
    Create KinderEgg component that, when visible, opens and shows a random students image for Mr Jerg's teacher-page

--- src/src/pages/FJerg/FJerg.tsx
+++ src/src/pages/FJerg/FJerg.tsx
@@ -1,12 +1,16 @@
 import * as React from "react";
 import Banner from "../../components/Banner/Banner";
+import KinderEgg from "./KinderEgg";
 
 import "./FJerg.scss";
 
 export default function FJerg() {
+    const kinderEggRef = React.createRef<HTMLDivElement>();
+
     return (
         <div className="f-jerg">
             <Banner text="Vielen Dank für das tolle Jahr mit Ihnen!"/>
+            <KinderEgg kinderEggRef={kinderEggRef}/>
         </div>
     )
 }
