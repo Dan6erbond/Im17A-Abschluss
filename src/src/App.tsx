5e28675a607ce993ca33c11368b81a8f1f9b4105
Dan6erbond <moravrav@gmail.com>
2020-05-16 07:15:26 +0200

    Fixes #37
    Add scroll back to top function on window.location pathname change

--- src/src/App.tsx
+++ src/src/App.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import {Route, Switch} from "react-router";
+import React, {useEffect} from 'react';
+import {Route, Switch, useLocation} from "react-router";
 import Home from "./pages/Home/Home";
 import {teachers} from "./teachers";
 
 function App() {
+    const { pathname } = useLocation();
+
+    useEffect(() => {
+        window.scrollTo(0, 0);
+    }, [pathname]);
+
     return (
         <Switch>
             <Route path="/" exact>
