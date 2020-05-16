4e76f29d03f56c1133a7705a4df14917dc95b9bf
Dan6erbond <moravrav@gmail.com>
2020-05-16 09:38:35 +0200

    Closes #10
    - Create Frankfurt slideshow component
    - Add images to public folder
    - Use Bootstrap carousel to display slideshow with Frankfurt images

--- src/src/pages/MGuignard/ClassTrip.tsx
+++ src/src/pages/MGuignard/ClassTrip.tsx
@@ -1,9 +1,26 @@
 import VizSensor from "react-visibility-sensor";
 import {motion} from "framer-motion";
-import {Button} from "react-bootstrap";
+import {Button, Carousel} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faBuilding} from "@fortawesome/free-solid-svg-icons";
 import * as React from "react";
+
+const images = [
+    "20180924_131103.jpg",
+    "20180924_155002.jpg",
+    "20180925_095942.jpg",
+    "20180925_102641.jpg",
+    "20180926_221012.jpg",
+    "20180926_221215.jpg",
+    "20180928_120908.jpg",
+    "IMG_2166.jpeg",
+    "IMG_2176.jpeg",
+    "IMG_2194.jpeg",
+    "IMG_2257.jpeg",
+    "IMG_20180924_131629.jpg",
+    "IMG_20180926_184642.jpg",
+    "IMG_20180927_094955.jpg"
+];
 
 export default function ClassTrip() {
     const scrollToRef = (ref: React.RefObject<HTMLDivElement>) => window.scrollTo(0, ref.current!!.offsetTop);
@@ -48,7 +65,22 @@ export default function ClassTrip() {
             </div>
 
             <div ref={classTripRef}>
-
+                <Carousel>
+                    {images.map((img, i) =>
+                        <Carousel.Item key={i}>
+                            <div>
+                                <img
+                                    className="d-block w-100"
+                                    src={`/res/img/mguignard/frankfurt/${img}`}
+                                    alt={img}
+                                />
+                            </div>
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>)}
+                </Carousel>
             </div>
         </div>
     );
