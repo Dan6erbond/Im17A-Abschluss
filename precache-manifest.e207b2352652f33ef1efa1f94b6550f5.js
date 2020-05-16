a36d893c5034fad56732fd09d6fd0fd56147cfea
Dan6erbond <moravrav@gmail.com>
2020-05-16 18:58:30 +0200

    Run build

---
+++ precache-manifest.e207b2352652f33ef1efa1f94b6550f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "612d9d8f6fd2e4c1e3ad8633b48c9a3c",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2230ad3298e53b5143fd",
+    "url": "./static/css/main.39a7f5bd.chunk.css"
+  },
+  {
+    "revision": "f242ef8550a485bf40cf",
+    "url": "./static/js/2.8f110b40.chunk.js"
+  },
+  {
+    "revision": "9466943c993e403794131be731926026",
+    "url": "./static/js/2.8f110b40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2230ad3298e53b5143fd",
+    "url": "./static/js/main.8f34e079.chunk.js"
+  },
+  {
+    "revision": "bb1f7d5f28c77e7c3a4d",
+    "url": "./static/js/runtime-main.b1dfa127.js"
+  }
+]);
