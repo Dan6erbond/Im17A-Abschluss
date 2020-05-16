f33e057eb56e4cb620b99680cef73ba483665950
Dan6erbond <moravrav@gmail.com>
2020-05-16 07:23:25 +0200

    Closes #23
    Create component for teacher-page-7 and add to teachers dictionary for rendering in App

---
+++ src/src/pages/FJerg/FJerg.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import Banner from "../../components/Banner/Banner";
+
+export default function FJerg() {
+    return (
+        <div className="f-jerg">
+            <Banner text="Vielen Dank für das tolle Jahr mit Ihnen!"/>
+        </div>
+    )
+}
