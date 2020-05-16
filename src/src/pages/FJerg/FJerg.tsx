65ac680b5a1ee6d4168f79ebd5c18669a5143b61
Dan6erbond <moravrav@gmail.com>
2020-05-16 18:37:20 +0200

    Replace simple Kinder egg with Selecta machine
    Create Selecta machine containing 9 eggs with random headshots and onSelect of an egg it gets dropped + larger

--- src/src/pages/FJerg/FJerg.tsx
+++ src/src/pages/FJerg/FJerg.tsx
@@ -1,21 +1,21 @@
 import * as React from "react";
 import {faEgg} from "@fortawesome/free-solid-svg-icons";
 import Banner from "../../components/Banner/Banner";
-import KinderEgg from "./KinderEgg";
 import ScrollButton from "../../components/ScrollButton/ScrollButton";
 
 import "./FJerg.scss";
+import Selecta from "./Selecta";
 
 export default function FJerg() {
-    const kinderEggRef = React.createRef<HTMLDivElement>();
+    const selectaRef = React.createRef<HTMLDivElement>();
 
     return (
         <div className="f-jerg">
             <Banner text="Vielen Dank für die tolle Zeit mit Ihnen!">
-                <ScrollButton scrollRef={kinderEggRef}
+                <ScrollButton scrollRef={selectaRef}
                               icon={faEgg}/>
             </Banner>
-            <KinderEgg kinderEggRef={kinderEggRef}/>
+            <Selecta selectaRef={selectaRef}/>
         </div>
     )
 }
--- src/src/pages/FJerg/KinderEgg.tsx
+++ src/src/pages/FJerg/KinderEgg.tsx
@@ -1,49 +1,16 @@
 import * as React from "react";
-import {Image, Layer, Stage} from "react-konva";
+import {Image, Layer} from "react-konva";
 import Konva from 'konva';
 import useImage from "use-image";
 import {Spring, animated} from 'react-spring/renderprops-konva';
-import VizSensor from "react-visibility-sensor";
-
-const students = [
-    {
-        name: "Ravi",
-        img: "./res/img/students/ravi.png"
-    },
-    {
-        name: "Alain",
-        img: "./res/img/students/alain.png"
-    },
-    {
-        name: "Albion",
-        img: "./res/img/students/albion.jpeg"
-    },
-    {
-        name: "Aron",
-        img: "./res/img/students/aron.jpg"
-    },
-    {
-        name: "Felix",
-        img: "./res/img/students/felix.jpg"
-    },
-    {
-        name: "Simon",
-        img: "./res/img/students/simon_2.png"
-    },
-    {
-        name: "Tim",
-        img: "./res/img/students/tim.jpeg"
-    }
-];
 
 interface KinderEggProps {
-    kinderEggRef: React.RefObject<HTMLDivElement>;
+    setComponent: (component: React.ReactNode | null) => void;
+    student: { name: string, img: string };
 }
 
 export default function KinderEgg(props: KinderEggProps) {
-    const {kinderEggRef} = props;
-
-    const [visible, setVisible] = React.useState<boolean>(false);
+    const {setComponent, student} = props;
 
     const [eggTop] = useImage(process.env.PUBLIC_URL + "/res/img/fjerg/kinder-ueberraschungsei_top.png");
     const eggTopWidth = 300;
@@ -55,79 +22,61 @@ export default function KinderEgg(props: KinderEggProps) {
 
     const eggTopRef = React.useRef<Konva.Image>(null);
 
-    const [student, setStudent] = React.useState<typeof students[0]>(students[Math.floor(Math.random() * students.length)]);
     const [studentImg] = useImage(student.img);
     const studentImgWidth = 100;
     const studentImgHeight = studentImg ? studentImg.height / studentImg.width * studentImgWidth : studentImgWidth;
 
     const [open, setOpen] = React.useState<boolean>(false);
-    const [closing, setClosing] = React.useState<boolean>(false);
 
     const openEgg = () => {
-        if (closing) return;
-
         setOpen(true);
-
         setTimeout(() => {
-            setOpen(false);
-            setClosing(true);
-            setTimeout(() => {
-                setStudent(students[Math.floor(Math.random() * students.length)]);
-                setClosing(false);
-            }, 500);
+            setComponent(null);
         }, 1000);
     };
 
     return (
-        <div className="kinder-egg" ref={kinderEggRef}>
-            <VizSensor partialVisibility onChange={isVisible => {
-                if (isVisible && !visible) {
-                    setVisible(true);
-                }
-            }}>
-                <Stage height={550} width={400}>
-                    <Layer>
-                        <Spring native
-                                to={{
-                                    x: open ? 230 : 75,
-                                    y: open ? 40 : 325,
-                                    opacity: open ? 1 : 0
-                                }}>
-                            {props => (
-                                <animated.Image {...props}
-                                                height={studentImgHeight}
-                                                width={studentImgWidth}
-                                                image={studentImg}/>
-                            )}
-                        </Spring>
-                    </Layer>
-                    <Layer>
-                        <Spring native
-                                to={{
-                                    x: open ? -70 : 50,
-                                    y: open ? 140 : 100,
-                                    rotation: open ? -40 : 0
-                                }}>
-                            {props => (
-                                <animated.Image {...props}
-                                                image={eggTop}
-                                                height={eggTopHeight}
-                                                width={eggTopWidth}
-                                                ref={eggTopRef}
-                                                onClick={openEgg}
-                                                onTouchStart={openEgg}/>
-                            )}
-                        </Spring>
-                        <Image x={47}
-                               y={193}
-                               image={eggBottom}
-                               height={eggBottomHeight}
-                               width={eggBottomWidth}
-                               onClick={openEgg}
-                               onTouchStart={openEgg}/>
-                    </Layer>
-                </Stage>
-            </VizSensor>
-        </div>
+        <React.Fragment>
+            <Layer>
+                <Spring native
+                        to={{
+                            x: open ? 280 : 125,
+                            y: open ? 90 : 375,
+                            opacity: open ? 1 : 0
+                        }}>
+                    {props => (
+                        <animated.Image {...props}
+                                        height={studentImgHeight}
+                                        width={studentImgWidth}
+                                        image={studentImg}/>
+                    )}
+                </Spring>
+            </Layer>
+            <Layer>
+                <Spring native
+                        to={{
+                            x: open ? -20 : 100,
+                            y: open ? 190 : 150,
+                            rotation: open ? -40 : 0
+                        }}>
+                    {props => (
+                        <animated.Image {...props}
+                                        image={eggTop}
+                                        height={eggTopHeight}
+                                        width={eggTopWidth}
+                                        ref={eggTopRef}
+                                        onClick={openEgg}
+                                        onTouchStart={openEgg}/>
+                    )}
+                </Spring>
+                <Image x={97}
+                       y={243}
+                       image={eggBottom}
+                       height={eggBottomHeight}
+                       width={eggBottomWidth}
+                       onClick={openEgg}
+                       onTouchStart={openEgg}/>
+            </Layer>
+        </React.Fragment>
     )
 }
