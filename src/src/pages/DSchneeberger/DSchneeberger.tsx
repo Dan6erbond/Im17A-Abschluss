9a3f1ef94b22f4d1aedf1b3dd335cc819b384037
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-05-17 23:29:38 +0200

    Implement Basic OneNote TopNav

--- src/src/pages/DSchneeberger/DSchneeberger.tsx
+++ src/src/pages/DSchneeberger/DSchneeberger.tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
+import Nav from "./OneNoteComponents/NavBar";
+import "./DSchneeberger.scss";
 
 function DSchneeberger() {
   return(
     <div className="d-schneeberger">
-        
+        <Nav/>
+        Bell made by <a href="https://www.flaticon.com/authors/freepik" title="Freepik">Freepik</a> from <a href="https://www.flaticon.com/" title="Flaticon"> www.flaticon.com</a>
     </div>
   );
 }
