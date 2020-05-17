99b9f80a91edceae63900e91b9d97c3ae92f852d
Dan6erbond <moravrav@gmail.com>
2020-05-17 09:38:01 +0200

    Remove placeholder text for plans
    Remove placeholder text for student plans, add plans for a student, made plans attribute optional

--- src/src/students.ts
+++ src/src/students.ts
@@ -2,7 +2,7 @@ export interface student {
     name: string;
     surname: string;
     img: string;
-    plans: string;
+    plans?: string;
     href?: string;
 }
 
@@ -10,20 +10,17 @@ export const students: student[] = [
     {
         name: "Alain",
         surname: "Siegrist",
-        img: "./res/img/students/alain.png",
-        plans: "I suck."
+        img: "./res/img/students/alain.png"
     },
     {
         name: "Albion",
         surname: "Spahija",
-        img: "./res/img/students/albion.png",
-        plans: "I suck."
+        img: "./res/img/students/albion.png"
     },
     {
         name: "Alexander",
         surname: "Greuter",
-        img: "./res/img/students/alex.png",
-        plans: "I suck."
+        img: "./res/img/students/alex.png"
     },
     {
         name: "Aron",
@@ -47,13 +44,12 @@ export const students: student[] = [
     {
         name: "Simon",
         surname: "Kunze",
-        img: "./res/img/students/simon.png",
-        plans: "I suck."
+        img: "./res/img/students/simon.png"
     },
     {
         name: "Tim",
         surname: "Mosbacher",
         img: "./res/img/students/tim.png",
-        plans: "I suck."
+        plans: "Nach dem Praktikum möchte ich meine Militärpflicht leisten, um nachher irgenwo, wahrscheinlich als Informatiker, zu arbeiten um eine eogene Wohnung zu leisten."
     }
 ];
