71ac761c1ea838e073442213d8bb5b4f502f6551
Dan6erbond <moravrav@gmail.com>
2020-05-17 10:17:31 +0200

    Make title and sub parameters optional

--- src/src/components/Slideshow/Slideshow.tsx
+++ src/src/components/Slideshow/Slideshow.tsx
@@ -2,7 +2,7 @@ import {Carousel} from "react-bootstrap";
 import * as React from "react";
 
 interface SlideshowProps {
-    images: { path: string, title: string, sub: string }[];
+    images: { path: string, title?: string, sub?: string }[];
     shuffle?: boolean;
 }
 
