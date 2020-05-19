dbf299c2c0e59490f9342374d02b6a091918a000
Dan6erbond <moravrav@gmail.com>
2020-05-19 13:40:41 +0200

    Add GET-FEEDBACK command
     - Add GET-FEEDBACK command with --f/-FORCE option
     - Adjust GET-MODULES to return  error messagewhen module doesn't exist

--- src/src/pages/RWeidmann/RWeidmann.tsx
+++ src/src/pages/RWeidmann/RWeidmann.tsx
@@ -27,62 +27,103 @@ const modules = [
     }
 ];
 
-const commandlets: Commandlet[] = [{
-    command: /GET-HELP/,
-    onRun: [
-        {
-            strings: [{value: "THEMA"}]
-        },
-        {
-            strings: [
-                {value: "\u00A0\u00A0\u00A0\u00A0"},
-                {value: "Hilfesystem zu Im17A PowerShell"}
-            ]
-        },
-        {
-            strings: [{value: "KURZBESCHREIBUNG"}]
-        },
-        {
-            strings: [
-                {value: "\u00A0\u00A0\u00A0\u00A0"},
-                {value: "Zeigt verfügbare Commandlets an."}
-            ]
-        },
-        {
-            strings: [{value: "GET-MODULES [MODULE]"}]
-        },
-        {
-            strings: [
-                {value: "\u00A0\u00A0\u00A0\u00A0"},
-                {value: "Zeigt alle im Unterricht behandelten Module an."}
-            ]
-        }
-    ]
-}, {
-    command: /GET-MODULES(?: (?:M)?(?<module>\d{3}))?/,
-    onRun: (result) => {
-        if (result?.groups?.module) {
+const feedback = [
+    {
+        student: "Simon Kunze",
+        feedback: "Es war sehr interessant, mit dem Arduino-Projekt einen Abstecher in die Embedded-Welt zu machen und mit PowerShell habe ich ein wichtiges Werkzeug für den Informatiker-Alltag erlernt."
+    },
+    {
+        student: "Albion Spahija",
+        feedback: "Das Arduino-Projekt war das grösste Highlight im Unterricht, ich konnte wertvolle Erfahrungen damit sammeln. Auch die anderen Module, zum Beispiel das Powershell-Modul hat mir sehr viel Freude bereitet. Das Server-Modul war doch ein bisschen anstrengend, doch es war trotzdem wichtig, dass wir dies ebenfalls angeschaut haben."
+    },
+    {
+        student: "Aron Eggenberger",
+        feedback: "Die Hilfestellungen waren oft sehr hilfreich und die Vorgaben meist klar, wenn nicht zu klar. Wir wurden oft unterstützt und die Modul-Themen wurden interessant als möglich präsentiert."
+    }
+];
+
+const commandlets: Commandlet[] = [
+    {
+        command: /GET-HELP/,
+        onRun: [
+            {
+                strings: [{value: "THEMA"}]
+            },
+            {
+                strings: [
+                    {value: "\u00A0\u00A0\u00A0\u00A0"},
+                    {value: "Hilfesystem zu Im17A PowerShell"}
+                ]
+            },
+            {
+                strings: [{value: "KURZBESCHREIBUNG"}]
+            },
+            {
+                strings: [
+                    {value: "\u00A0\u00A0\u00A0\u00A0"},
+                    {value: "Zeigt verfügbare Commandlets an."}
+                ]
+            },
+            {
+                strings: [{value: "GET-MODULES [MODULE]"}]
+            },
+            {
+                strings: [
+                    {value: "\u00A0\u00A0\u00A0\u00A0"},
+                    {value: "Zeigt alle im Unterricht behandelten Module an."}
+                ]
+            }
+        ]
+    },
+    {
+        command: /GET-MODULES(?: (?:M)?(?<module>\d{3}))?/,
+        onRun: (result) => {
+            if (result?.groups?.module) {
+                for (let i = 0; i < modules.length; i++) {
+                    let module = modules[i];
+
+                    if (module.nr == result.groups.module) {
+                        return [
+                            {strings: [{value: `M${module.nr}`}]},
+                            {strings: [{value: "\u00A0\u00A0\u00A0\u00A0"}, {value: `${module.name}`}]},
+                        ];
+                    }
+                }
+
+                return [{strings: [{value: `Modul M${result.groups.module} konnte nicht gefunden werden`}]}];
+            }
+
+            let lines = [];
             for (let i = 0; i < modules.length; i++) {
                 let module = modules[i];
-
-                if (module.nr == result?.groups?.module) {
-                    return [
-                        {strings: [{value: `M${module.nr}`}]},
-                        {strings: [{value: "\u00A0\u00A0\u00A0\u00A0"}, {value: `${module.name}`}]},
-                    ];
-                }
+                lines.push({strings: [{value: `M${module.nr}`}]});
+                lines.push({strings: [{value: "\u00A0\u00A0\u00A0\u00A0"}, {value: `${module.name}`}]});
             }
-        }
+            return lines;
+        },
+    },
+    {
+        command: /GET-FEEDBACK( )?(?<student>\w*(( ){1}\w+)?)( )?( )?(?<force>--f|-FORCE)?/,
+        onRun: (result) => {
+            if (result?.groups?.student) {
+                for (let i = 0; i < feedback.length; i++) {
+                    let fb = feedback[i];
 
-        let lines = [];
-        for (let i = 0; i < modules.length; i++) {
-            let module = modules[i];
-            lines.push({strings: [{value: `M${module.nr}`}]});
-            lines.push({strings: [{value: "\u00A0\u00A0\u00A0\u00A0"}, {value: `${module.name}`}]});
+                    if (fb.student.toLowerCase().includes(result.groups.student.toLowerCase())) {
+                        return [
+                            {strings: [{value: `Feedback von ${fb.student}`}]},
+                            {strings: [{value: "\u00A0\u00A0\u00A0\u00A0"}, {value: `${result.groups.force ? fb.force || fb.feedback : fb.feedback}`}]},
+                        ];
+                    }
+                }
+
+                return [{strings: [{value: `Feedback von ${result.groups.student} konnte nicht gefunden werden`}]}];
+            }
+
+            return [{strings: [{value: `Bitte geben Sie einen Schülernamen für das Feedback mit`}]}];
         }
-        return lines;
     }
-}];
+];
 
 const lines = [{
     strings: [{
