a5c28fa75e3823ccb8238fcf9d57357ab0c56969
Dan6erbond <moravrav@gmail.com>
2020-05-20 19:37:01 +0200

    Add banner

--- src/src/pages/LKlink/LKlink.tsx
+++ src/src/pages/LKlink/LKlink.tsx
@@ -1,11 +1,13 @@
 import * as React from "react";
 
 import "./LKlink.scss";
+import FrankfurtSlideshow from "../../components/Slideshow/Frankfurt/FrankfurtSlideshow";
+import Banner from "../../components/Banner/Banner";
 
 export default function LKlink() {
     return (
         <div className="l-klink">
-
+            <Banner text=""/>
         </div>
     );
 }
