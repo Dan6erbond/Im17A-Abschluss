0566165667bf3c9ece24723742b2e44efc5e1bf5
Dan6erbond <moravrav@gmail.com>
2020-05-21 19:56:55 +0200

    run build

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.8253dd200421fc55e3a8a3717b262733.js"
+  "./precache-manifest.bbee2235ca0c4267ab359c210a30fa5e.js"
 );
 
 self.addEventListener('message', (event) => {
