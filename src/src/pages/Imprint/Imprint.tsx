c96ffe9d366b7c216cd44edc66ea418c13d9f6db
Dan6erbond <moravrav@gmail.com>
2020-05-22 10:53:34 +0200

    Create basic imprint page
     - Create basic imprint page
     - Move Barlow font to index as global font

---
+++ src/src/pages/Imprint/Imprint.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+import {Container} from "react-bootstrap";
+
+export default function Imprint() {
+    return (
+        <Container fluid="md" className="imprint">
+            <br/>
+            <h2>Impressum</h2>
+        </Container>
+    );
+}
