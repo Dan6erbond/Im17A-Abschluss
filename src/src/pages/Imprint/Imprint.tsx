434a90eb89e1e03bc6aaf0a351fd26e011b815fe
Dan6erbond <moravrav@gmail.com>
2020-05-22 11:04:47 +0200

    Add imprint contents
    Add about section in imprint and credits, including authors of libraries and styles used

--- src/src/pages/Imprint/Imprint.tsx
+++ src/src/pages/Imprint/Imprint.tsx
@@ -1,11 +1,43 @@
 import * as React from "react";
 import {Container} from "react-bootstrap";
 
+import "./Imprint.scss";
+
 export default function Imprint() {
     return (
         <Container fluid="md" className="imprint">
             <br/>
+            <h2>About</h2>
+            <p>
+                Diese Seite wurde als Abschiedsgeschenk für die Lehrer der IMS-Klasse I3A erstellt.
+            </p>
             <h2>Impressum</h2>
+            <p>
+                Diese Website wurde erstellt von:
+            </p>
+            <p>
+                RaviAnand Mohabir
+                <br/>
+                Miescherweg 3,
+                <br/>
+                4812 Mühlethal
+                <br/>
+                Schweiz
+                <br/>
+            </p>
+            <p>
+                Dominik Berger
+            </p>
+            <h3>Credits</h3>
+            <p>
+                <b>Layout:</b> React-Bootstrap wurde für das allgemeine Layout eingesetzt.
+            </p>
+            <p>
+                <b>Icons:</b> Die meisten Icons stammen aus dem Fontawesome Icon-Repository.
+            </p>
+            <p>
+                <b>Fonts:</b> Alle eingesetzten Fonts, sind auf Google Fonts zu finden.
+            </p>
         </Container>
     );
 }
