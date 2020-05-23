6134ebb8da5769b98b05b8d008fc0ee392d5fe1f
Dan6erbond <moravrav@gmail.com>
2020-05-23 21:05:26 +0200

    Add onclick events to parallax

--- src/src/pages/AFlick/AFlick.tsx
+++ src/src/pages/AFlick/AFlick.tsx
@@ -1,30 +1,32 @@
 import * as React from "react";
-import {Container} from "react-bootstrap";
+import NavBar from "./NavBar";
+import {Parallax, ParallaxLayer} from "react-spring/renderprops-addons";
 
 import "./AFlick.scss";
-import {Parallax, ParallaxLayer} from "react-spring/renderprops-addons";
-import NavBar from "./NavBar";
 
 export default function AFlick() {
+    const parallaxRef = React.createRef<Parallax>();
+
     return (
         <div className="a-flick">
             <NavBar/>
-            <Parallax pages={3.5}>
+            <Parallax ref={parallaxRef} pages={3.5}>
                 <ParallaxLayer offset={0} speed={1}>
                     <div style={{
                         height: '100%',
                         width: '100%',
                         backgroundImage: `url('/res/img/aflick/html-code.jpg')`,
                         backgroundSize: 'cover'
-                    }}/>
+                    }} onClick={() => parallaxRef.current?.scrollTo(0.8)}/>
                 </ParallaxLayer>
                 <ParallaxLayer offset={1} speed={-0.5}>
-                    <div className="title">
+                    <div className="title" onClick={() => parallaxRef.current?.scrollTo(0.8)}>
                         <h2>Web-Dev</h2>
                     </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={1.3} speed={0.1}>
-                    <Container fluid="md">
+                    <div className="container-md" style={{height: '100%'}}
+                         onClick={() => parallaxRef.current?.scrollTo(1.6)}>
                         <blockquote className="blockquote">
                             <p className="mb-0">
                                 Dank Ihrer Einführung in Web-Design habe ich mein Portfolio als Webseite erstellt und
@@ -34,7 +36,7 @@ export default function AFlick() {
                                 RaviAnand Mohabir
                             </footer>
                         </blockquote>
-                    </Container>
+                    </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={1.9} speed={0.3}>
                     <div className="title">
@@ -42,16 +44,18 @@ export default function AFlick() {
                     </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={2} speed={0.1}>
-                    <Container fluid="md">
+                    <div className="container-md" style={{height: '100%'}}
+                         onClick={() => parallaxRef.current?.scrollTo(2.99)}>
                         <blockquote className="blockquote">
                             [Insert Quote]
                         </blockquote>
-                    </Container>
+                    </div>
                 </ParallaxLayer>
                 <ParallaxLayer offset={2.99} speed={0.4}>
-                    <Container fluid="md">
+                    <div className="container-md" style={{height: '100%'}}
+                         onClick={() => parallaxRef.current?.scrollTo(0)}>
                         <h3 style={{textAlign: 'center'}}>Vielen Dank für den spannenden Unterricht mit Ihnen!</h3>
-                    </Container>
+                    </div>
                 </ParallaxLayer>
             </Parallax>
         </div>
