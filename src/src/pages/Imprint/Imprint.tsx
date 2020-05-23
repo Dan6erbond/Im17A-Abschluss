ecc06e9b1d5f64cb9f14043bc3239801d4599657
Dan6erbond <moravrav@gmail.com>
2020-05-23 14:39:07 +0200

    Add credits for individual teacher pages

--- src/src/pages/Imprint/Imprint.tsx
+++ src/src/pages/Imprint/Imprint.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import {Container} from "react-bootstrap";
+import {Col, Container, Row} from "react-bootstrap";
 
 import "./Imprint.scss";
 import {Link} from "react-router-dom";
+import {teachers} from "../../teachers";
 
 export default function Imprint() {
     return (
@@ -39,6 +40,35 @@ export default function Imprint() {
             <p>
                 <b>Fonts:</b> Alle eingesetzten Fonts, sind auf Google Fonts zu finden.
             </p>
+            <h4>Pages</h4>
+            <Container>
+                <Row>
+                    <Col md={6} sm={12}>
+                        <b>Dominik Berger</b>
+                        <ul>
+                            {["Daniel Schneeberger", "Geneviève Gross", "Sascha Fiechter", "Loredana Arleo", "Matthias Graf", "Andreas Neeser"].sort().map((teacher, index) =>
+                                <li key={index}>
+                                    <Link
+                                        to={`/${teachers.filter(t => t.name === teacher)[0]?.path}`}>
+                                        {teacher}
+                                    </Link>
+                                </li>)}
+                        </ul>
+                    </Col>
+                    <Col md={6} sm={12}>
+                        <b>RaviAnand Mohabir</b>
+                        <ul>
+                            {["Lil Klink", "Alexander Flick", "Réne Weidmann", "Lars Meyer", "Marielle Guignard", "Fabian Jerg"].sort().map((teacher, index) =>
+                                <li key={index}>
+                                    <Link
+                                        to={`/${teachers.filter(t => t.name === teacher)[0]?.path}`}>
+                                        {teacher}
+                                    </Link>
+                                </li>)}
+                        </ul>
+                    </Col>
+                </Row>
+            </Container>
             <br/>
             <Link style={{float: 'right'}} to="/">« Zurück</Link>
         </Container>
