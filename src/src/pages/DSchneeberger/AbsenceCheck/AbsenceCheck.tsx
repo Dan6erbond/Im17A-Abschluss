0232366494a070765a55cf36cd55251b50f433ba
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-05-24 00:19:06 +0200

    Closes #36
    Basic  Implementation of Absence Check Game,
    Serge is missing

--- src/src/pages/DSchneeberger/AbsenceCheck/AbsenceCheck.tsx
+++ src/src/pages/DSchneeberger/AbsenceCheck/AbsenceCheck.tsx
@@ -2,47 +2,67 @@ import * as React from "react";
 import Student from "./Student";
 import {Pupil} from "./Student";
 
+const serge = "SERGE";
+
 const pupils = [
   {
     message: "Präsent",
-    initials: "DB",
+    initials: "DB", //Dominik
     colour: "#f5c43d"
   },
   {
     message:"Anwesend",
-    initials:"RM",
-    colour: "red"
+    initials:"RM", //Ravi
+    colour: "#d3b49c"
   },
   { message: "ich bin hier",
-    initials: "AG",
-    colour: "yellow"
+    initials: "AG", //Alex
+    colour: "#d1b49d"
   },
-  { message: "test",
-    initials: "TM",
-    colour: "yellow"
+  { message: "Da",
+    initials: "TM", //Tim
+    colour: "#f4b4b4"
   },
-  { message: "test",
-    initials: "AS",
-    colour: "yellow"
+  { message: "Bereit",
+    initials: "AS", //Alain
+    colour: "#d9dcdf"
   },
-  { message: "test",
-    initials: "SK",
-    colour: "yellow"
+  { message: "Hier",
+    initials: "AS", //Albion
+    colour: "#b0bad4"
+  },
+  { message: "Dabei",
+    initials: "SK", //Simon
+    colour: "#b5afdc"
+  },
+  { message: "Ich bin auch da!",
+    initials: "AE", //Aron
+    colour: "#b4addd"
+  },
+  { message: "Hier",
+    initials: "FF", //Felix
+    colour: "#f4d474"
   }
 ];
 
 interface AbsenceCheckProps{
 
 }
+
 interface AbsenceCheckState{
-    pupils: Pupil[]
+    pupils: Pupil[],
+    input: string;
+    submittedAnswer: boolean;
+    correct: boolean;
 }
 
 class AbsenceCheck extends React.Component<AbsenceCheckProps, AbsenceCheckState>{
   constructor(props: AbsenceCheckProps){
     super(props);
     this.shuffleArray = this.shuffleArray.bind(this);
-    this.state = {pupils: pupils.shuffle()};
+    this.checkInput = this.checkInput.bind(this);
+
+    this.state = {pupils: pupils.shuffle(), input: "", submittedAnswer: false, correct: false};
   }
 
   public shuffleArray(){
@@ -50,14 +70,34 @@ class AbsenceCheck extends React.Component<AbsenceCheckProps, AbsenceCheckState>
     return pupils;
   }
 
+  public checkInput(){
+    this.setState({submittedAnswer:true})
+    let inputInUpper = this.state.input.toUpperCase();
+    if(inputInUpper === serge){
+      this.setState({correct: true});
+    }
+  }
+
   render(){
     return(
+      !this.state.submittedAnswer ?
       <div className="content">
       <div className="content-title">
         <h2>Wer fehlt?</h2>
         <span>Mittwoch, 20.05.2020 16:41</span>
       </div>
         {Array.from(Array(this.state.pupils.length).keys()).map(i => <Student key={i} student={this.state.pupils[i]}/>)}
+        <br/>
+        <p>Eine Person fehlt. Wer ist es?</p>
+        <input type="text" onChange={_e => {this.setState({input : _e.target.value})}}/>
+        <br/><br/>
+        <input type="button" value="Überprüfen"  onClick={this.checkInput}/>
+        <br/><br/>
+      </div>
+      :
+      <div className="content">
+        <p>{this.state.correct ? "Stimmt. Serge fehlt! ;)" : "Nein, leider falsch."}</p>
+        <input type="button" value="Nochmal" onClick={() => this.setState({submittedAnswer:false, input:"", correct: false})}/>
       </div>
     );
   }
--- src/src/pages/DSchneeberger/AbsenceCheck/Student.tsx
+++ src/src/pages/DSchneeberger/AbsenceCheck/Student.tsx
@@ -12,6 +12,7 @@ export interface StudentProps{
 
 interface StudentState{
   student: Pupil;
+  clicked: boolean;
 }
 
 
@@ -20,19 +21,37 @@ class Student extends React.Component<StudentProps, StudentState>{
     super(props);
 
     this.state = {
-        student: props.student
+        student: props.student, clicked: false
     };
+
+    this.handleStudentClick = this.handleStudentClick.bind(this);
+  }
+
+  handleStudentClick(){
+    this.setState({clicked: true});
   }
 
   render(){
-    return(
+      if(this.state.clicked){
+        return(
         <div className="content-body">
           <div className="content-text">
+            <p  style={{border: `${this.state.student.colour} solid 3.5px`}}>{this.state.student.message} ✔</p>
+            <span>{this.state.student.initials}</span>
+          </div>
+        </div>
+        );
+      }else{
+        return(
+        <div className="content-body" >
+          <div className="content-text" onClick={this.handleStudentClick}>
             <p style={{border: `${this.state.student.colour} solid 3.5px`}}>{this.state.student.message}</p>
             <span>{this.state.student.initials}</span>
           </div>
         </div>
-    );
+        );
+      }
+
   }
 
 }
