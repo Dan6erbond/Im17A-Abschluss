e47942a7d62d4da84eb514ac24612f3eb7db5f95
Dan6erbond <moravrav@gmail.com>
2020-05-24 10:03:12 +0200

    Implement parallax layout
    Implement parallax scrolling on Ms Klink's page

--- src/src/pages/LKlink/LKlink.tsx
+++ src/src/pages/LKlink/LKlink.tsx
@@ -3,12 +3,26 @@ import * as React from "react";
 import "./LKlink.scss";
 import FrankfurtSlideshow from "../../components/Slideshow/Frankfurt/FrankfurtSlideshow";
 import Banner from "../../components/Banner/Banner";
+import NavBar from "../AFlick/NavBar";
+import {Parallax, ParallaxLayer} from "react-spring/renderprops-addons";
 
 export default function LKlink() {
+    const parallaxRef = React.createRef<Parallax>();
+
     return (
         <div className="l-klink">
-            <Banner text=""/>
-            <FrankfurtSlideshow/>
+            <Parallax ref={parallaxRef} pages={2}>
+                <ParallaxLayer offset={0.4} speed={1}>
+                    <h2 onClick={() => parallaxRef.current?.scrollTo(1)}>
+                        <span style={{textDecoration: 'line-through'}}>Klassenlehrerin</span>
+                        <br/>
+                        <span>Vertrauensperson</span>
+                    </h2>
+                </ParallaxLayer>
+                <ParallaxLayer offset={1} speed={0.3}>
+                    <FrankfurtSlideshow/>
+                </ParallaxLayer>
+            </Parallax>
         </div>
     );
 }
