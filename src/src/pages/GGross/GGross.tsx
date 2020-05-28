0616d6d43bacc18ae12375c4c4b205705e4c615e
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-05-28 10:40:23 +0200

    Basic Implementation of parallax
    Added first offset page and fade in title for said page

--- src/src/pages/GGross/GGross.tsx
+++ src/src/pages/GGross/GGross.tsx
@@ -1,12 +1,36 @@
 import * as React from "react";
+import {Parallax, ParallaxLayer} from 'react-spring/renderprops-addons'
 
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faArrowCircleDown} from "@fortawesome/free-solid-svg-icons";
+
+import Banner from "../../components/Banner/Banner";
 
 import "./GGross.scss";
 
 function GGross() {
+  const ref = React.createRef<Parallax>();
+  const greetingText = "Vielen Dank für die tolle Zeit mit Ihnen!";
   return(
     <div className="g-gross">
-        <p>Hello World</p>
+      <Parallax pages={3} scrolling={true} ref={ref}>
+        <ParallaxLayer offset={0} speed={0.1}>
+            <Banner text={greetingText}>
+              <div onClick={() => ref.current!!.scrollTo(1)}>
+                <FontAwesomeIcon className="icon" icon={faArrowCircleDown} style={{height:"55px", width:"55px"}}/>
+                <p>Scrollen</p>
+              </div>
+            </Banner>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={0.5}>
+        <div className="laaxopen" />
+        </ParallaxLayer>
+        <ParallaxLayer offset={1.1} speed={-0.1}>
+          <div className="memes">
+            <h2>Juicy Memes</h2>
+          </div>
+        </ParallaxLayer>
+      </Parallax>
     </div>
   );
 }
