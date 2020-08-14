95d526198a306646ea1afd2ff7be104af7067047
Doemuu <43069409+Doemuu@users.noreply.github.com>
2020-08-14 14:07:51 +0200

    Closes #2

--- src/src/pages/GGross/GGross.tsx
+++ src/src/pages/GGross/GGross.tsx
@@ -4,6 +4,8 @@ import Gallery from "react-photo-gallery";
 import MemeCard from './Memes/MemeCard';
 import {meme} from './Memes/meme';
 
+import {Meme} from "./Memes/meme";
+
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faArrowCircleDown} from "@fortawesome/free-solid-svg-icons";
 
@@ -18,14 +20,22 @@ interface GGrossProps{
 
 interface GGrossState{
   fullscreen: boolean;
+  chosenMeme?: Meme;
 }
 
 class GGross extends React.Component<GGrossProps, GGrossState>{
   constructor(props: GGrossProps){
     super(props);
-    this.state={fullscreen: false}
+    this.state={
+      fullscreen: false,
+      chosenMeme: undefined
+    }
   }
 
+  handleMemeClick(meme : Meme){
+    console.log(meme);
+    this.setState({chosenMeme: meme, fullscreen: true});
+  }
 
 render(){
   const ref = React.createRef<Parallax>();
@@ -33,7 +43,7 @@ render(){
   return(
     !this.state.fullscreen ?
     <div className="g-gross">
-      <Parallax pages={4} scrolling={true} ref={ref}>
+      <Parallax pages={3} scrolling={true} ref={ref}>
         <ParallaxLayer offset={0} speed={0.1}>
             <Banner text={greetingText}>
               <div onClick={() => ref.current!!.scrollTo(1)}>
@@ -42,31 +52,58 @@ render(){
               </div>
             </Banner>
         </ParallaxLayer>
-        <ParallaxLayer offset={2.1} speed={-0.1}>
+        <ParallaxLayer offset={1} speed={0.1}>
           <div className="memes">
             <h2>Juicy Memes</h2>
-            <div className="gallery">
-              <Gallery photos={meme} margin={5} onClick={() => this.setState({fullscreen: true})}/>
+            <div className="row">
+              <div className="column">
+              {meme.map((m, index) =>
+                index < meme.length/2 ?
+                <img src={m.src} onClick={() => this.handleMemeClick(m)}/>
+                :
+                null
+              )}
+              </div>
+              <div className="column">
+              {meme.map((m, index) =>
+                index >= meme.length/2 ?
+                <img src={m.src} onClick={() => this.handleMemeClick(m)}/>
+                :
+                null
+              )}
+              </div>
             </div>
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.1} speed={0.2}>
-          <div className="trampoline">
-            <h2>Trampolin</h2>
-          </div>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.2} speed={-0.2}>
-            <div className="scrollBtn" onClick={() => ref.current!!.scrollTo(2.1)}>
+
+            <div onClick={() => ref.current!!.scrollTo(2)}>
               <FontAwesomeIcon className="icon" icon={faArrowCircleDown} style={{height:"55px", width:"55px"}}/>
               <p>Scrollen</p>
             </div>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2.3} speed={0.2}>
+          <div className="footnote">
+            <h2>Not So Juicy Footnote</h2>
+            <div>
+              <p>Sehr geehrte Frau Gross. Nach dem Unabhängigkeitskrieg von 2019, in dem die IMS
+              den WMS Tyrannen endlich das Handwerk legte, waren Sie unsere Sportlehrerin geworden. Das Ziel eines Homogenen Ethnoturunterricht war bereis seit Jahren in
+              unseren Herzen tief verwurzelt. Wir hatten unglaublich viel Spass und waren froh von nun an im engsten Kreis den Unterricht schwänzen zu können. Mit Ihrer Hilfe
+              wurde der Sportunterricht erträglich und wir konnten uns innerhalb der Klasse weiterentwickeln.</p>
+              <p>Wir möchten Ihnen Danken für Ihren unerschütterlichen Einsatz uns zu motivieren und zu fördern.</p>
+              <p>Best regards</p>
+              <p>Ehemalige I3a</p>
+            </div>
+          </div>
         </ParallaxLayer>
       </Parallax>
     </div>
     :
     <div className="meme-content" onClick={() => this.setState({fullscreen : false})}>
       <div className="fullscreen">
-        <MemeCard meme={meme[0]} />
+      {this.state.chosenMeme != undefined ?
+        <MemeCard meme={this.state.chosenMeme} />
+        :
+        <p>There is no Meme chosen</p>
+      }
       </div>
   </div>
   );
--- src/src/pages/GGross/Memes/meme.ts
+++ src/src/pages/GGross/Memes/meme.ts
@@ -1,57 +1,42 @@
 export interface Meme {
     src: string;
-    height: number;
-    width: number;
-    key: string;
 }
 
 export const meme: Meme[] = [
     {
-      src: '/res/img/ggross/memeImg/alain.png',
-      height: 1,
-      width: 1,
-      key:"1"
+      src: '/res/img/ggross/memeImg/meme1.jpg'
     },
     {
-      src: '/res/img/ggross/memeImg/alain.png',
-      height: 2,
-      width: 3,
-      key:"2"
+      src: '/res/img/ggross/memeImg/meme2.jpg'
     },
     {
-      src: '/res/img/ggross/memeImg/alain.png',
-      height: 2,
-      width: 3,
-      key:"3"
+      src: '/res/img/ggross/memeImg/meme3.jpg'
     },
     {
-      src: '/res/img/ggross/memeImg/alain.png',
-      height: 2,
-      width: 3,
-      key:"4"
+      src: '/res/img/ggross/memeImg/meme4.jpg'
     },
     {
-      src: '/res/img/ggross/memeImg/alain.png',
-      height: 2,
-      width: 3,
-      key:"5"
+      src: '/res/img/ggross/memeImg/meme5.jpg'
     },
     {
-      src: '/res/img/ggross/memeImg/alain.png',
-      height: 2,
-      width: 3,
-      key:"6"
+      src: '/res/img/ggross/memeImg/meme6.jpg'
     },
     {
-      src: '/res/img/ggross/memeImg/alain.png',
-      height: 2,
-      width: 3,
-      key:"7"
+      src: '/res/img/ggross/memeImg/meme7.jpg'
     },
     {
-      src: '/res/img/ggross/memeImg/alain.png',
-      height: 2,
-      width: 3,
-      key:"8"
+      src: '/res/img/ggross/memeImg/meme8.jpg'
+    },
+    {
+      src: '/res/img/ggross/memeImg/meme9.jpg'
+    },
+    {
+      src: '/res/img/ggross/memeImg/meme10.jpg'
+    },
+    {
+      src: '/res/img/ggross/memeImg/meme11.jpg'
+    },
+    {
+      src: '/res/img/ggross/memeImg/meme12.jpg'
     }
 ];
